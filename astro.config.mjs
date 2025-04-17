2cccad16048c4a079fcb12492c22638dbe15ca9d
Quan Nguyen <quan.nguyen@elsanow.io>
2025-04-17 14:37:29 +0700

    Config adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,26 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { LOCALES } from './src/constains';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
   integrations: [react()],
+
   i18n: {
     locales: LOCALES,
     defaultLocale: 'en',
   },
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   prefetch: {
     prefetchAll: true
-  }
+  },
+
+  adapter: netlify()
 });
